e62da48df1ded3df442b5af06cf87e320b63d4ab
GranOla18 <80288848+GranOla18@users.noreply.github.com>
2022-04-07 05:40:44 -0500

    Birthday Color Generator // RGB Values
    Added 2 new buttons and 2 new textViews:
    - A button por setting the user's birthday, this is done by displaying a calenday letting the user choose any date
    - A button for changing the backgorund color to the user's BD color
    - A TextView for looking at the selected BD, or by default, it stablishes the current day when no BD is set
    - A TextView for the RGB values of the background color

--- app/src/main/java/com/axinpz/proyectosegundoparcial/colorGeneratorActivity.kt
+++ app/src/main/java/com/axinpz/proyectosegundoparcial/colorGeneratorActivity.kt
@@ -1,33 +1,77 @@
 package com.axinpz.proyectosegundoparcial
 
-import android.content.Intent
+import android.app.DatePickerDialog
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
-import java.lang.IllegalArgumentException
+import java.text.SimpleDateFormat
+import java.util.*
 import kotlin.random.Random
 
 class colorGeneratorActivity : AppCompatActivity(), View.OnClickListener {
 
     var layout: ConstraintLayout? = null
 
+    private var txtVBD: TextView? = null
+    private var btnBD: Button? = null
+    private var btnBDColor: Button? = null
+
+    private var txtVRGBVals: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_color_generator)
+
+        txtVBD = findViewById(R.id.txtV_BD)
+        btnBD = findViewById(R.id.btn_setBD)
+        btnBDColor = findViewById(R.id.btn_BDColor)
+        txtVRGBVals = findViewById(R.id.txtV_rgb_vals)
+
+        val myCalendar = Calendar.getInstance()
+        val datePicker = DatePickerDialog.OnDateSetListener{view, year, month, dayOfMonth ->
+            myCalendar.set(Calendar.YEAR, year)
+            myCalendar.set(Calendar.MONTH, month)
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateLabel(myCalendar)
+        }
 
         val btnRandomColor = findViewById<Button>(R.id.btn_random_color)
         layout = findViewById<ConstraintLayout>(R.id.color_layout)
 
         btnRandomColor.setOnClickListener(this)
+
+        btnBD?.setOnClickListener{
+            DatePickerDialog(this, datePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show()
+        }
+
+        btnBDColor?.setOnClickListener{
+            val redCom = myCalendar.get((Calendar.DAY_OF_MONTH)) * 8
+            val greenCom = myCalendar.get((Calendar.MONTH)) * 19
+            val blueCom = (((myCalendar.get((Calendar.YEAR))) - 1000) / 8) + 25
+
+            val bdColor = Color.rgb(redCom, greenCom, blueCom)
+            layout?.setBackgroundColor(bdColor)
+            txtVRGBVals?.setText(redCom.toString() + ", " + greenCom.toString() + ", " + blueCom.toString())
+            updateLabel(myCalendar)
+        }
+    }
+
+    private fun updateLabel(myCalendar: Calendar) {
+        val myFormat = "dd-MM-yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.US)
+        txtVBD?.setText(sdf.format(myCalendar.time))
     }
 
     override fun onClick(view: View) {
-        val random = Random.nextInt(0, 256)
-
-        val color = Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256))
+        val redCom = Random.nextInt(256)
+        val greenCom = Random.nextInt(256)
+        val blueCom = Random.nextInt(256)
+        val color = Color.rgb(redCom, greenCom, blueCom)
         layout?.setBackgroundColor(color)
+        txtVRGBVals?.setText(redCom.toString() + ", " + greenCom.toString() + ", " + blueCom.toString())
     }
 }
