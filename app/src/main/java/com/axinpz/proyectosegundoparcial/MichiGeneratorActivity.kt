149bb41cfe143975c3fb791a009e6104769dd6fe
GranOla18 <80288848+GranOla18@users.noreply.github.com>
2022-04-06 23:59:37 -0500

    Michi Generator Done
    Added 10 michi images, an images array and a random function that will pick an index of this array and set it on the ImageView

--- app/src/main/java/com/axinpz/proyectosegundoparcial/MichiGeneratorActivity.kt
+++ app/src/main/java/com/axinpz/proyectosegundoparcial/MichiGeneratorActivity.kt
@@ -2,10 +2,40 @@ package com.axinpz.proyectosegundoparcial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import kotlin.random.Random
 
-class MichiGeneratorActivity : AppCompatActivity() {
+class MichiGeneratorActivity : AppCompatActivity(), View.OnClickListener {
+
+    val images = arrayOf(
+        R.drawable.michi1,
+        R.drawable.michi2,
+        R.drawable.michi3,
+        R.drawable.michi4,
+        R.drawable.michi5,
+        R.drawable.michi6,
+        R.drawable.michi7,
+        R.drawable.michi8,
+        R.drawable.michi9,
+        R.drawable.michi10,
+    )
+
+    var imageView: ImageView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_michi_generator)
+
+        imageView = findViewById<ImageView>(R.id.imageView)
+        val btnMichi = findViewById<Button>(R.id.btn_get_michi)
+
+        btnMichi.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        val imageIdx = Random.nextInt(11)
+        imageView?.setImageResource(images[imageIdx])
     }
 }
