368bf30aa383be8b1b00bd16145fac6389f65d0b
AlexisM <0204545@up.edu.mx>
2022-04-07 01:42:13 -0500

    YouTubeVideoPlayer.feat
    Se agregó en PlayVideoActivity.kt la funcionalidad de poder reproducir un video de YouTube al tocar el botón "Play Video" en la Main Activity

--- app/src/main/java/com/axinpz/proyectosegundoparcial/PlayVideoActivity.kt
+++ app/src/main/java/com/axinpz/proyectosegundoparcial/PlayVideoActivity.kt
@@ -2,10 +2,106 @@ package com.axinpz.proyectosegundoparcial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.tabs.TabLayout
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
 
-class PlayVideoActivity : AppCompatActivity() {
+const val YOUTUBE_VIDEO_ID_KEY = "Hkz0NcKPzMs"
+
+class PlayVideoActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
+    val TAG = "YoutubePlayerActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_play_video)
+
+        val layout =  layoutInflater.inflate(R.layout.activity_play_video, null) as ConstraintLayout
+        setContentView(layout)
+
+        val playerView = YouTubePlayerView(this)
+        playerView.layoutParams =  ConstraintLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        layout.addView(playerView)
+
+        playerView.initialize(getString(R.string.GOOGLE_API_KEY), this)
+
     }
+
+    override fun onInitializationSuccess(provider: YouTubePlayer.Provider?, player: YouTubePlayer?, wasRestored: Boolean) {
+        Log.d(TAG, "onInitializationSucces")
+        Toast.makeText(this, "Initialized Successfully", Toast.LENGTH_LONG).show()
+
+        player?.setPlaybackEventListener(playbackEventListener)
+        player?.setPlayerStateChangeListener(changeStateListener)
+
+        if(!wasRestored) {
+            player?.cueVideo(YOUTUBE_VIDEO_ID_KEY)
+        }
+    }
+
+    override fun onInitializationFailure(provider: YouTubePlayer.Provider?, youTubeInitializationResult: YouTubeInitializationResult?) {
+        Log.d(TAG, "onInitializationFailure")
+        val REQUEST_CODE = 0
+        if(youTubeInitializationResult?.isUserRecoverableError == true) {
+            youTubeInitializationResult.getErrorDialog(this, REQUEST_CODE).show()
+        } else {
+            Toast.makeText(this,"Error starting plaer", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private val playbackEventListener = object : YouTubePlayer.PlaybackEventListener {
+        override fun onPlaying() {
+            Toast.makeText(this@PlayVideoActivity,"Playing", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onPaused() {
+            Toast.makeText(this@PlayVideoActivity,"Paused", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onStopped() {
+
+        }
+
+        override fun onBuffering(p0: Boolean) {
+
+        }
+
+        override fun onSeekTo(p0: Int) {
+
+        }
+
+    }
+
+    private val changeStateListener = object : YouTubePlayer.PlayerStateChangeListener {
+        override fun onLoading() {
+
+        }
+
+        override fun onLoaded(p0: String?) {
+
+        }
+
+        override fun onAdStarted() {
+            Toast.makeText(this@PlayVideoActivity,"Add", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onVideoStarted() {
+
+        }
+
+        override fun onVideoEnded() {
+            Toast.makeText(this@PlayVideoActivity,"Finished", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onError(p0: YouTubePlayer.ErrorReason?) {
+
+        }
+
+    }
+
 }
